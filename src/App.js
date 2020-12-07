57cd923a2b39f9e9b0b38c6fceb06fd41d84396d
xerxes23 <josephmendoza23@gmail.com>
2020-12-07 13:44:05 -0500

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -18,13 +18,6 @@ function App() {
 
   return (
     <div className="App">
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossorigin="anonymous"
-        å
-      />
       <h1> Eigentlon </h1>
       {/* <Button color="primary" variant="contained">
         Login
